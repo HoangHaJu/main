ea467972f68898629dcb870ef8c6632bf493afe0
HoangHaJu <hoangthth0984@gmail.com>
2025-01-13 22:09:44 +0700

    update_code

---
+++ www.tesol.org/Static/javascript/scripts/upcomingEvents55cf.js
@@ -0,0 +1,9 @@
+﻿// This script removes empty section for Upcoming Events block, 
+//when there are no events to show.
+$(function () {
+    $("section.content.component.usn_cmp_upcomingevents .component-inner .container-fluid").each(function (index, element) {
+        if ($(element).children().length <= 0) {
+            $(element).parents("section.content.component.usn_cmp_upcomingevents").remove();
+        }
+    });
+});
---
+++ www.tesol.org/Static/javascript/scripts/url-current-page-cookie8417.js
@@ -0,0 +1,3 @@
+﻿if (window.location.pathname != "/member-log-in-message/" && window.location.pathname != "/members-log-in/") {
+    document.cookie = `currentUrl=${window.location.pathname};path=/`;
+}
